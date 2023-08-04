db11da754806c27264bb46b19bad4fe76f1e1d27
Camilo Bravo <hola@cambraca.com>
2023-08-03 22:57:39 -0500

    Volatil album page (bandcamp & distrokid)

--- volatil/settings.php
+++ volatil/settings.php
@@ -13,13 +13,14 @@
 	}
 
 	$links = array(
-		'bandcamp'		=> new Link('mailto:hola@cambraca.com', 'Bandcamp'),
-		'spotify'		=> new Link('https://musicians.today/@cambraca', 'Spotify'),
-		'soundcloud'	=> new Link('https://soundcloud.com/cambraca', 'SoundCloud'),
-		'youtube'		=> new Link('https://www.youtube.com/cambraca1', 'YouTube'),
-		'apple'			=> new Link('https://www.youtube.com/cambraca1', 'Apple Music'),
-		'amazon'		=> new Link('https://www.youtube.com/cambraca1', 'Amazon Music'),
-		'pandora'		=> new Link('https://www.youtube.com/cambraca1', 'Pandora'),
-		'deezer'		=> new Link('https://www.youtube.com/cambraca1', 'Deezer'),
-		'tidal'			=> new Link('https://www.youtube.com/cambraca1', 'Tidal'),
+		'bandcamp'		=> new Link('https://cambraca.bandcamp.com/album/volatil', 'Bandcamp'),
+		'distrokid'		=> new Link('https://distrokid.com/hyperfollow/camilobravo/volatil', 'DistroKid'),
+//		'spotify'		=> new Link('https://musicians.today/@cambraca', 'Spotify'),
+//		'soundcloud'	=> new Link('https://soundcloud.com/cambraca', 'SoundCloud'),
+//		'youtube'		=> new Link('https://www.youtube.com/cambraca1', 'YouTube'),
+//		'apple'			=> new Link('https://www.youtube.com/cambraca1', 'Apple Music'),
+//		'amazon'		=> new Link('https://www.youtube.com/cambraca1', 'Amazon Music'),
+//		'pandora'		=> new Link('https://www.youtube.com/cambraca1', 'Pandora'),
+//		'deezer'		=> new Link('https://www.youtube.com/cambraca1', 'Deezer'),
+//		'tidal'			=> new Link('https://www.youtube.com/cambraca1', 'Tidal'),
 	);
